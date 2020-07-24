4c86d034b300bca1f93461641c78725f27bfc888
avaghasia-dw <62565186+avaghasia-dw@users.noreply.github.com>
2020-07-24 15:05:23 -0500

    missed positionChecker module

---
+++ positionChecker/positionCheck.py
@@ -0,0 +1,12 @@
+
+
+
+class PositionChecker(object):
+    def __init__(self):
+        print('created!')
+
+    def checkPosition(self, image):
+        print('thanks for the image!');
+        results = {}
+        results['posture'] = "straight"
+        return results
